9cf9577a1e007b22e425f3c521546441a6c61860
Pavel Halama <pavel.halama@siemens.com>
2021-05-07 09:12:23 +0200

    code cleaning - exports

---
+++ config_file_handler.js
@@ -0,0 +1,104 @@
+
+
+const createOnboardingConfig = (deviceConfigObj) => {
+  
+    var obj = { 
+      "localUserName": "", 
+      "localPassword": "", 
+      "deviceName": "", 
+      "deviceTypeId": ""
+    };
+    const map = new Map();
+    let keys = Object.keys(obj)
+    for (let index = 1; index < Object.values(deviceConfigObj).length; index++) {
+      map.set(keys[index-1], Object.values(deviceConfigObj)[index]);
+    }
+  
+    return Object.fromEntries(map);
+  };
+
+  
+  const createNetworkConfig = (networkConfigObj) => {
+    //console.log(networkConfigObj["NUMBER OF INTERFACES*"]);
+    let interfaces = []; 
+    // basic network configuration 
+    var basicNetwork = { 
+      "MacAddress": "",
+      "GatewayInterface": true,
+      "DHCP": "",
+    };
+  
+    for (let j = 0; j < networkConfigObj["NUMBER OF INTERFACES*"]; j++) {
+      const map = new Map();
+      let keys = Object.keys(basicNetwork)
+      for (let i = 3; i <= 5; i++) {
+        map.set(keys[i-3], Object.values(networkConfigObj)[i].toString().split(',')[j]);
+      }
+      interfaces.push(Object.fromEntries(map))
+    }
+    // DHCP configuration 
+    var static = {
+      "IPv4": "",
+      "NetMask": "",
+      "Gateway": ""
+    }
+  
+    let dhcp = networkConfigObj["DHCP*"].toString().split(',');
+  
+    for (let j = 0; j < dhcp.length; j++) {
+      if (dhcp[j]== "disabled") {
+        const map = new Map();
+        let keys = Object.keys(static)
+        for (let i = 6; i <= 8; i++) {
+          map.set(keys[i-6], Object.values(networkConfigObj)[i].toString().split(',')[j]);
+        }
+        Object.assign(interfaces[j],{"Static" : Object.fromEntries(map)})
+      } else {
+        Object.assign(interfaces[j],{"Static": static});
+      };
+    };
+  
+    // DNS configuration 
+    let primary_dns = networkConfigObj["PRIMARY DNS*"].toString().split(',');
+    let secondary_dns = networkConfigObj["SECONDARY DNS*"].toString().split(',');
+  
+    let primary_ind = [];
+    let primary_true_count = 0; 
+    let secondary_ind = [];
+    let secondary_true_count = 0; 
+  
+    for (let i = 0; i < secondary_dns.length; i++) {
+      var dns_config = {
+        "PrimaryDNS": "",
+        "SecondaryDNS": ""
+      };
+      Object.assign(interfaces[i],{"DNSConfig" : dns_config})
+  
+      if (secondary_dns[i].toString().trim()  == "true") {
+        secondary_ind.push(i);
+        secondary_true_count++; 
+      }
+      if (primary_dns[i].toString().trim() == "true") {
+        primary_ind.push(i);
+        primary_true_count++; 
+      }
+      
+    };
+      
+  
+    let primary_dns_ip = networkConfigObj["PRIMARY DNS IP*"].toString().split(',');
+    let secondary_dns_ip = networkConfigObj["SECONDARY DNS IP*"].toString().split(',');
+  
+  
+    for (let j = 0 ; j < primary_true_count; j++) {
+      interfaces[primary_ind[j]]["DNSConfig"]["PrimaryDNS"] = primary_dns_ip[j].toString();
+    };
+    for (let j = 0 ; j < secondary_true_count; j++) {
+      interfaces[secondary_ind[j]]["DNSConfig"]["SecondaryDNS"] = secondary_dns_ip[j].toString();
+    };
+    return interfaces;
+  };
+  
+
+  exports.createOnboardingConfig = createOnboardingConfig; 
+  exports.createNetworkConfig = createNetworkConfig; 
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require("axios").default;
 const fs = require('fs');
 const FormData = require('form-data');
 const xlsx = require('xlsx');
-
+const handler = require('./config_file_handler.js')
 
 // Enviroment Variables 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
@@ -32,137 +32,15 @@ const proxy_config = xlsx.utils.sheet_to_json(file.Sheets[file.SheetNames[5]]);
 //console.log(typeof network_config[0]["MAC ADRESS*"]);
 
 var adresses = network_config[0]["MAC ADRESS*"].split(',');
-//console.log(adresses);
+
 
 // creating config files 
 
-// device config sheet
-const createOnboardingConfig = (deviceConfigObj) => {
-  
-  var obj = { 
-    "localUserName": "", 
-    "localPassword": "", 
-    "deviceName": "", 
-    "deviceTypeId": ""
-  };
-  const map = new Map();
-  let keys = Object.keys(obj)
-  for (let index = 1; index < Object.values(deviceConfigObj).length; index++) {
-    map.set(keys[index-1], Object.values(deviceConfigObj)[index]);
-  }
-
-  return Object.fromEntries(map);
-};
-
-//console.log(createOnboardingConfig(device_config[0]))
+console.log(handler.createOnboardingConfig(device_config[0]))
 
 // network config sheet
 
-console.log(network_config[0]);
-
-var object = {
-  "MacAddress": "00:0C:29:FC:EA:3E",
-  "GatewayInterface": true,
-  "DHCP": "disabled",
-  "Static": {
-      "IPv4": "192.168.1.108",
-      "NetMask": "255.255.255.0",
-      "Gateway": "192.168.1.1"
-  },
-  "DNSConfig": {
-    "PrimaryDNS": "192.168.1.1",
-    "SecondaryDNS": "8.8.8.8"
-  },
-  "L2Conf": {
-    "StartingAddressIPv4": "192.168.1.8",
-    "NetMask": "255.255.255.0",
-    "Range": "2"
-  }
-};
-
-const createNetworkConfig = (networkConfigObj) => {
-  //console.log(networkConfigObj["NUMBER OF INTERFACES*"]);
-  let interfaces = []; 
-  // basic network configuration 
-  var basicNetwork = { 
-    "MacAddress": "",
-    "GatewayInterface": true,
-    "DHCP": "",
-  };
-
-  for (let j = 0; j < networkConfigObj["NUMBER OF INTERFACES*"]; j++) {
-    const map = new Map();
-    let keys = Object.keys(basicNetwork)
-    for (let i = 3; i <= 5; i++) {
-      map.set(keys[i-3], Object.values(networkConfigObj)[i].toString().split(',')[j]);
-    }
-    interfaces.push(Object.fromEntries(map))
-  }
-  // DHCP configuration 
-  var static = {
-    "IPv4": "",
-    "NetMask": "",
-    "Gateway": ""
-  }
-
-  let dhcp = networkConfigObj["DHCP*"].toString().split(',');
-
-  for (let j = 0; j < dhcp.length; j++) {
-    if (dhcp[j]== "disabled") {
-      const map = new Map();
-      let keys = Object.keys(static)
-      for (let i = 6; i <= 8; i++) {
-        map.set(keys[i-6], Object.values(networkConfigObj)[i].toString().split(',')[j]);
-      }
-      Object.assign(interfaces[j],{"Static" : Object.fromEntries(map)})
-    } else {
-      Object.assign(interfaces[j],{"Static": static});
-    };
-  };
-
-  // DNS configuration 
-  let primary_dns = networkConfigObj["PRIMARY DNS*"].toString().split(',');
-  let secondary_dns = networkConfigObj["SECONDARY DNS*"].toString().split(',');
-
-  let primary_ind = [];
-  let primary_true_count = 0; 
-  let secondary_ind = [];
-  let secondary_true_count = 0; 
-
-  for (let i = 0; i < secondary_dns.length; i++) {
-    var dns_config = {
-      "PrimaryDNS": "",
-      "SecondaryDNS": ""
-    };
-    Object.assign(interfaces[i],{"DNSConfig" : dns_config})
-
-    if (secondary_dns[i].toString().trim()  == "true") {
-      secondary_ind.push(i);
-      secondary_true_count++; 
-    }
-    if (primary_dns[i].toString().trim() == "true") {
-      primary_ind.push(i);
-      primary_true_count++; 
-    }
-    
-  };
-    
-
-  let primary_dns_ip = networkConfigObj["PRIMARY DNS IP*"].toString().split(',');
-  let secondary_dns_ip = networkConfigObj["SECONDARY DNS IP*"].toString().split(',');
-
-
-  for (let j = 0 ; j < primary_true_count; j++) {
-    interfaces[primary_ind[j]]["DNSConfig"]["PrimaryDNS"] = primary_dns_ip[j].toString();
-  };
-  for (let j = 0 ; j < secondary_true_count; j++) {
-    interfaces[secondary_ind[j]]["DNSConfig"]["SecondaryDNS"] = secondary_dns_ip[j].toString();
-  };
-  return interfaces;
-};
-
-
-console.log(createNetworkConfig(network_config[0]));
+console.log(handler.createNetworkConfig(network_config[0]));
 
 
 
