de1270388e2e6cb945807bcdf99bd2cffe7a37a7
Pavel Halama <pavel.halama@siemens.com>
2022-05-05 09:08:45 +0200

    update based on sw clearing

--- src/upload_over_ui/index.js
+++ src/upload_over_ui/index.js
@@ -1,32 +1,29 @@
 const express = require('express');
 const fs = require("fs")
 const multer = require('multer');
-const path = require('path');
-const uuid = require('uuid').v4;
 const app = express();
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
+    destination: function (req, file, cb) {
         cb(null, 'devices')
     },
-    filename: (req, file, cb) => {
-        const { originalname } = file;
-        // or 
-        // uuid, or fieldname
-        cb(null, originalname);
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
     }
 })
-const upload = multer({ storage }); // or simply { dest: 'uploads/' }
+// upload using multer
+const upload = multer({ storage });
+// host static html site
 app.use(express.static('public'))
 
 app.post('/upload', upload.array('avatar'), (req, res) => {
     if (fs.existsSync("./devices/edge_devices.xlsx")) {
-        //file exists
         res.send("<html><body><h1  style='color:green;text-align:center;margin-top: 2em;'> File edge_devices.xlsx has been uploaded</h1></body></html>")
-
     } else {
         res.send("<html><body><h1   style='color:red;text-align:center;margin-top: 2em;'> The file edge_devices.xlsx is not located in the /devices folder. Try again.</h1></body></html>")
     }
 });
 
 
-app.listen(3001);
+app.listen(3001, () => {
+    console.log("Listening on port 3001");
+})
